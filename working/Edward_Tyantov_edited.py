a2fc8094dddb5984cd31833e8b07ed8aa4c401f8
George Batchkala <george.batchkala@gmail.com>
2019-10-27 18:04:33 +0000

    correct MJ's surname

--- working/Edward_Tyantov_edited.py
+++ working/Edward_Tyantov_edited.py
@@ -28,7 +28,7 @@
 
 
 # The code was published without a licence and publically available on the author's GitHub page without any licence.
-# It was initially based on the code by Marko Jocič (later MJ), which is confirmed by ET in the very end of
+# It was initially based on the code by Marko Jocić (later MJ), which is confirmed by ET in the very end of
 # the README.md file. The code by MJ was published under the MIT licence. You can find this code on the author's
 # GitHub page: https://github.com/jocicmarko/ultrasound-nerve-segmentation/
 
